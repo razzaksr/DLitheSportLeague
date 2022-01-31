18a00e4c8eac1cba41ddd2363a593e7a0124991c
razzaksr <razzaksr@gmail.com>
2022-01-31 13:04:34 +0530

    Projects are created

---
+++ DLitheTournament/src/main/java/sports/dlithe/tournaments/log/DLitheTournament/DLitheTournamentApplication.java
@@ -0,0 +1,13 @@
+package sports.dlithe.tournaments.log.DLitheTournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLitheTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DLitheTournamentApplication.class, args);
+	}
+
+}
---
+++ DLitheTournament/src/test/java/sports/dlithe/tournaments/log/DLitheTournament/DLitheTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package sports.dlithe.tournaments.log.DLitheTournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLitheTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
